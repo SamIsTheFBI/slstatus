c26d1950311989cfa6ef1b9b0e15717ea9bd0e33
SamIsTheRetard <mrqube001@gmail.com>
2022-04-09 15:24:09 +0530

    ~

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const struct arg args[] = {
 	/* function		format          argument */
         { netspeed_rx,		"▼%sB/s |",  "wlan0"},
         { netspeed_tx,		"▲%sB/s |",  "wlan0"},
-        { ram_perc,  		"  %s%% |",     NULL},
+        { ram_perc,  		" %s |",     NULL},
         { cpu_perc, 		" CPU %s%% |",  NULL},
         { run_command, 		" %s |",        "battery"},
         { datetime, 		" %s ",         "%a %d %b %Y | %R:%S" }
